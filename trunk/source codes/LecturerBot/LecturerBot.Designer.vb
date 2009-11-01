f5746c0ba5aa3f81e4cd09acc6a8bf09b6384249
lab4games <lab4games@40cc54bc-b367-11de-8ef2-6d788aed22ac>
2009-11-01 11:52:42 +0000

    bug fix

--- trunk/source codes/LecturerBot/LecturerBot.Designer.vb
+++ trunk/source codes/LecturerBot/LecturerBot.Designer.vb
@@ -28,12 +28,12 @@ Partial Class LecturerBot
         Me.Edventure = New System.Windows.Forms.Timer(Me.components)
         Me.sdk = New AxAWSDKLib42.AxAwSdk42
         Me.ActiveWorld = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox
         CType(Me.sdk, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Connection
         '
-        Me.Connection.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Connection.Location = New System.Drawing.Point(0, 0)
         Me.Connection.MinimumSize = New System.Drawing.Size(20, 20)
         Me.Connection.Name = "Connection"
@@ -56,22 +56,33 @@ Partial Class LecturerBot
         'ActiveWorld
         '
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 576)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(760, 176)
+        Me.TextBox1.TabIndex = 9
+        '
         'LecturerBot
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(784, 564)
+        Me.ClientSize = New System.Drawing.Size(784, 764)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Connection)
         Me.Controls.Add(Me.sdk)
         Me.Name = "LecturerBot"
         Me.Text = "LecturerBot"
         CType(Me.sdk, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Connection As System.Windows.Forms.WebBrowser
     Friend WithEvents Edventure As System.Windows.Forms.Timer
     Friend WithEvents sdk As AxAWSDKLib42.AxAwSdk42
     Friend WithEvents ActiveWorld As System.Windows.Forms.Timer
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
 
 End Class
